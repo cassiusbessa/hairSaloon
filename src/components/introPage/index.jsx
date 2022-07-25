80d57d500538c1812371bbf993c9b0f97aaea669
Jhony Altoé <jhonymikealtoe@hotmail.com>
2022-07-25 12:40:46 -0300

    feat: adiciona componente verticalCenterModal e base para um formulário
    Foi instalado a biblioteca pra auxiliar na construção dos componentes e inserido mais um componente (verticalCenterModal) e um formuláriofoi adicionado como base para uso futuro. Também foram movidas as classes responsaveis pelos botões da página para um escopo global tendo em vista que seria utilizado mais vezes

--- src/components/introPage/index.jsx
+++ src/components/introPage/index.jsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import { useState } from 'react';
 import './style.scss'
+import Form from 'react-bootstrap/Form';
+
+import MyVerticallyCenteredModal from '../verticalCenterModal';
 
 
 function IntroPage() {
+  const [modalShow, setModalShow] = useState(false);
+
   return (
-    <div id='introPage'>
-      <button className='shiny-button'>
+    <div id ='introPage'>
+      <button className='default-page-button' variant="primary" onClick={() => setModalShow(true)}>
         Agende o seu horário
       </button>
+
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+        title='Agendamento'
+        additionalCustonButton= {
+          <button variant="primary" type="submit" className='default-page-button'>
+            Submit
+          </button>
+        }
+      >
+        <Form>
+          <Form.Group>
+            <Form.Label>Email:</Form.Label>
+            <Form.Control type="email" placeholder="Digite seu email" />
+            <Form.Text className="text-muted">
+              Nós nunca enviaremos seu email para ninguém.
+            </Form.Text>
+          </Form.Group>
+        </Form>
+      </MyVerticallyCenteredModal>
     </div>
   );
 }
---
+++ src/components/verticalCenterModal/index.jsx
@@ -0,0 +1,27 @@
+import Modal from 'react-bootstrap/Modal';
+
+function MyVerticallyCenteredModal (props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          {props.title}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        {props.children}
+      </Modal.Body>
+      <Modal.Footer>
+        <button onClick={props.onHide} className='default-page-button'>Close</button>
+        {props.additionalCustonButton}
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default MyVerticallyCenteredModal;
